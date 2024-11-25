36111c608e497c5bf4c0a3b356f964c31357f0db
Mary <wasteofspace@rocketmail.com>
2024-11-25 01:58:58 -0500

    Assignment 4 resubmission with actual injection rather than hacks.

--- assignment4-autorentals-it/autorentals-it-docker/src/test/java/info/ejava_student/starter/assignment4/docker/IntegrationTestConfiguration.java
+++ assignment4-autorentals-it/autorentals-it-docker/src/test/java/info/ejava_student/starter/assignment4/docker/IntegrationTestConfiguration.java
@@ -2,8 +2,10 @@ package info.ejava_student.starter.assignment4.docker;
 
 import info.ejava.examples.common.web.ServerConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -11,15 +13,20 @@ import java.net.URI;
 //@SpringBootConfiguration
 //@EnableAutoConfiguration
 @Slf4j
+@Configuration
+@ConfigurationProperties("it.server")
 public class IntegrationTestConfiguration {
+    @Value("${it.server.host:}")
+    private String host;
+
+    @Value("${it.server.port:}")
+    private String port;
 
     @Bean
-    @ConfigurationProperties("it.server")
     ServerConfig serverConfig() {
-    log.info("IT server port as configured: {}",System.getProperty("it.server.port"));
+        log.info("IT server port as configured: {}",port);
     ServerConfig serverConfig = new ServerConfig("http",
-            System.getProperty("it.server.host"),
-            Integer.parseInt(System.getProperty("it.server.port")),
+            host, Integer.parseInt(port),
             null,null,null);
     log.info("Host {}, Port {}, baseURL {}",serverConfig.getHost(),serverConfig.getPort(),serverConfig.getBaseUrl());
         return serverConfig;
